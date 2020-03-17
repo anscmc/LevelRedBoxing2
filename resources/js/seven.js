9578c2b580c05b08571afac401ff9648f4208f01
anscmc <an_sc_mc@yahoo.com>
2020-03-17 01:47:36 -0400

    virtual page

---
+++ resources/js/seven.js
@@ -0,0 +1,48 @@
+//var attempt = 3; //Variable to count number of attempts.
+
+// Below function Executes on click of login button.
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "test" && password == "test"){
+       hideLogin();
+        await sleep(750);
+        showWorkout();
+        return false;
+}
+else if ( username == "admin" && password == "admin123"){
+        hideLogin();
+        await sleep(750);
+        showWorkout();
+        return false;
+}
+else if ( username == "pitt" && password == "pitt"){
+       hideLogin();
+        await sleep(750);
+        showWorkout();
+        return false;
+}
+else{
+    $("p.errormsg").addClass("shstore");
+//attempt --;// Decrementing by one.
+//alert("Incorrect username and/or password. Please try again.");
+// Disabling fields after 3 attempts.
+//if( attempt == 0){
+//document.getElementById("username").disabled = true;
+//document.getElementById("password").disabled = true;
+//document.getElementById("submit").disabled = true;
+//return false;
+//}
+}
+}
+
+function hideLogin(){
+    $("div.parallax1").fadeToggle(750);
+}
+function showWorkout(){
+    $("div.parallax2").fadeToggle(1000);
+}
+
